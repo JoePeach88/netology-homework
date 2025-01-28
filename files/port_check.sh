217cc9e520d8335b42f147f75b9e8394aed707b1
Egor Gaev <gaev5941@gmail.com>
2025-01-28 09:52:09 +0300

    updated port check script

--- files/port_check.sh
+++ files/port_check.sh
@@ -2,5 +2,5 @@
 if [[ $(netstat -tuln | grep LISTEN | grep :80) ]] && [[ -f /var/www/html/index.nginx-debian.html ]]; then
         exit 0
 else
-        sudo systemctl stop keepalived
+        exit 1
 fi
